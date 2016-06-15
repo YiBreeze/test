dec991bf3e31aa2c7d9388bdf23de344aaa1c7cf
L <liuguotao@topscore.com.cn>
2016-06-15 10:49:40 +0800

    create

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by L on 16/6/15.
+//  Copyright © 2016年 L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
